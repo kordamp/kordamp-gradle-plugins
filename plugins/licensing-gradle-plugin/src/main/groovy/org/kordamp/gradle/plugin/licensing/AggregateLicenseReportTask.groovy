35c2fd92ef4b369a548964243fc7529260a2f2bd
Andres Almiray <aalmiray@gmail.com>
2020-11-12 16:58:06 +0100

    [licensing] Add HTML report for aggregate license task. Fixes #421 Bonus: added JSON reports

--- plugins/licensing-gradle-plugin/src/main/groovy/org/kordamp/gradle/plugin/licensing/AggregateLicenseReportTask.groovy
+++ plugins/licensing-gradle-plugin/src/main/groovy/org/kordamp/gradle/plugin/licensing/AggregateLicenseReportTask.groovy
@@ -17,8 +17,10 @@
  */
 package org.kordamp.gradle.plugin.licensing
 
+import groovy.json.JsonOutput
 import groovy.transform.Canonical
 import groovy.transform.CompileStatic
+import groovy.xml.MarkupBuilder
 import groovy.xml.StreamingMarkupBuilder
 import groovy.xml.XmlUtil
 import nl.javadude.gradle.plugins.license.DownloadLicensesExtension
@@ -61,20 +63,104 @@ class AggregateLicenseReportTask extends DefaultTask {
             }
         }
 
+        allDependencies = allDependencies.sort({ it.value.file.text() })
+
         File reportFile = project.file("${reportDir}/aggregate-dependency-license.xml")
         reportFile.parentFile.mkdirs()
         reportFile.text = XmlUtil.serialize(new StreamingMarkupBuilder().bind {
             dependencies {
-                allDependencies.sort({ it.value.file.text() }).each { name, dep ->
+                allDependencies.each { name, dep ->
                     dependency(name: dep.@name) {
                         file(dep.file.text())
-                        dep.license.each { lic ->
+                        dep.license.sort().each { lic ->
                             license(name: lic.@name, url: lic.@url)
                         }
                     }
                 }
             }
         })
+
+        reportFile = project.file("${reportDir}/aggregate-dependency-license.html")
+        reportFile.withPrintWriter { writer ->
+            MarkupBuilder html = new MarkupBuilder(writer)
+
+            html.html {
+                head {
+                    title("HTML License report")
+                }
+                style(
+                        '''table {
+                  width: 85%;
+                  border-collapse: collapse;
+                  text-align: center;
+                }
+                .dependencies {
+                  text-align: left;
+                }
+                tr {
+                  border: 1px solid black;
+                }
+                td {
+                  border: 1px solid black;
+                  font-weight: bold;
+                  color: #2E2E2E
+                }
+                th {
+                  border: 1px solid black;
+                }
+                h3 {
+                  text-align:center;
+                  margin:3px
+                }
+                .license {
+                    width:70%
+                }
+
+                .licenseName {
+                    width:15%
+                }
+                ''')
+                body {
+                    table(align: 'center') {
+                        tr {
+                            th() { h3("Dependency") }
+                            th() { h3("Jar") }
+                            th() { h3("License name") }
+                            th() { h3("License text URL") }
+                        }
+
+                        allDependencies.each { name, dep ->
+                            dep.license.sort().each { lic ->
+                                tr {
+                                    td(dep.@name, class: 'dependencies')
+                                    td(dep.file.text(), class: 'licenseName')
+                                    td(lic.@name, class: 'licenseName')
+                                    td(class: 'license') {
+                                        if (lic.@url) {
+                                            a(href: lic.@url, "Show license agreement")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        reportFile = project.file("${reportDir}/aggregate-dependency-license.json")
+        reportFile.text = JsonOutput.toJson([dependencies: allDependencies.values().collect([]) { dep ->
+            [
+                    name    : String.valueOf(dep.@name),
+                    file    : String.valueOf(dep.file.text()),
+                    licenses: dep.license.sort().collect([]) { lic ->
+                        [
+                                name: String.valueOf(lic.@name),
+                                url : String.valueOf(lic.@url)
+                        ]
+                    }
+            ]
+        }])
     }
 
     private void computeAggregateByLicense() {
@@ -98,17 +184,108 @@ class AggregateLicenseReportTask extends DefaultTask {
             }
         }
 
+        allLicenses = allLicenses.sort({ it.key })
+
         File reportFile = project.file("${reportDir}/aggregate-license-dependency.xml")
         reportFile.parentFile.mkdirs()
         reportFile.text = XmlUtil.serialize(new StreamingMarkupBuilder().bind {
             licenses {
-                allLicenses.sort({ it.key }).each { name, lic ->
+                allLicenses.each { name, lic ->
                     license(name: lic.name, url: lic.url) {
                         lic.artifacts.sort().each { artifact -> dependency(artifact) }
                     }
                 }
             }
         })
+
+        reportFile = project.file("${reportDir}/aggregate-license-dependency.html")
+        reportFile.withPrintWriter { writer ->
+            MarkupBuilder html = new MarkupBuilder(writer)
+
+            html.html {
+                head {
+                    title("HTML License report")
+                }
+                style(
+                        '''table {
+                  width: 85%;
+                  border-collapse: collapse;
+                  text-align: center;
+                }
+
+                .dependencies {
+                  text-align: left;
+                  width:15%;
+                }
+
+                tr {
+                  border: 1px solid black;
+                }
+
+                td {
+                  border: 1px solid black;
+                  font-weight: bold;
+                  color: #2E2E2E
+                }
+
+                th {
+                  border: 1px solid black;
+                }
+
+                h3 {
+                  text-align:center;
+                  margin:3px
+                }
+
+                .license {
+                    width:70%
+                }
+
+                .licenseName {
+                    width:15%
+                }
+                ''')
+                body {
+                    table(align: 'center') {
+                        tr {
+                            th() { h3("License") }
+                            th() { h3("License text URL") }
+                            th() { h3("Dependency") }
+                        }
+
+                        allLicenses.each { name, lic ->
+                            tr {
+                                td(lic.name, class: 'licenseName')
+                                td(class: 'license') {
+                                    if (lic.url) {
+                                        a(href: lic.url, "License agreement")
+                                    }
+                                }
+                                td(class: "dependencies") {
+                                    ul() {
+                                        lic.artifacts.sort().each {
+                                            dependency ->
+                                                li(dependency)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        reportFile = project.file("${reportDir}/aggregate-license-dependency.json")
+        reportFile.text = JsonOutput.toJson([licences: allLicenses.values().collect([]) { lic ->
+            [
+                    name        : String.valueOf(lic.@name),
+                    url         : String.valueOf(lic.@url),
+                    dependencies: lic.artifacts.sort().collect([]) { artifact ->
+                        String.valueOf(artifact)
+                    }
+            ]
+        }])
     }
 
     @Canonical
