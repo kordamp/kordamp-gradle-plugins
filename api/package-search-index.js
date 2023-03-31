61052c72292c89645d1081888c95fa336211ef95
Andres Almiray <aalmiray@gmail.com>
2023-03-31 13:45:59 +0200

    Publish guide for unspecified

--- api/package-search-index.js
+++ api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.kordamp.gradle.plugin.clirr.reporters"},{"l":"org.kordamp.gradle.plugin.clirr.tasks"},{"l":"org.kordamp.gradle.plugin.test.tasks"},{"l":"org.kordamp.gradle.util"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.kordamp.gradle.plugin.test.tasks"},{"l":"org.kordamp.gradle.util"}]
